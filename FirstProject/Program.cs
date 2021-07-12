23ac5daea8c222b5ab9824f6f52f7bb17548a1f8
TerezaSimkova <tereza.simkova251@gmail.com>
2021-07-12 13:26:28 +0200

    second phrase

--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -7,6 +7,7 @@ namespace FirstProject
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("My name is Tereza.");
         }
     }
 }
